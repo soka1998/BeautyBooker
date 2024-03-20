cf9c2e67643eefa166ff08e0f7cb3e33305ca1ed
soka1998 <soukainaharifi18@gmail.com>
2024-03-20 00:31:25 +0000

    create staff middleware

--- server/routes/serviceRoutes.js
+++ server/routes/serviceRoutes.js
@@ -1,14 +1,16 @@
 import express from "express";
 import { addService, deleteService, getAllServices, getServiceById, updateService } from "../controllers/serviceController.js";
 import requireAdmin from "../middleware/adminMiddleware.js";
+import requireStaff from '../middleware/staffMiddleware.js';
+import requireAuth from "../middleware/authMiddleware.js";
 
 const serviceRoute= express.Router()
 
-serviceRoute.get('/:serviceId',getServiceById)
-serviceRoute.get('/',getAllServices)
-serviceRoute.post('/',requireAdmin,/*staffMiddlewrae*/ addService)
-serviceRoute.put('/:serviceId',requireAdmin,/*staffMiddlewrae*/ updateService)
-serviceRoute.delete('/:serviceId',requireAdmin,/*staffMiddlewrae*/ deleteService)
+serviceRoute.get('/:serviceId',requireStaff,requireAuth,getServiceById)
+serviceRoute.get('/',requireStaff,requireAuth,getAllServices)
+serviceRoute.post('/',requireAdmin,requireStaff, addService)
+serviceRoute.put('/:serviceId',requireAdmin,requireStaff, updateService)
+serviceRoute.delete('/:serviceId',requireAdmin,requireStaff, deleteService)
 
 
 export default serviceRoute;
