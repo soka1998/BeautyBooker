699c15636905a30ede76bf89ca5ff91ded846438
soka1998 <soukainaharifi18@gmail.com>
2024-03-25 23:01:44 +0000

    update about page

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -1,29 +1,29 @@
+// AboutUs.js
 import React from "react";
-import image from "../assets/aboutimg.jpg";
+import image from "../assets/aboutimg.jpg"; // Make sure to use the correct path
 
 const AboutUs = () => {
   return (
-    <>
-      <section className="container mx-auto px-4">
-        <h2 className="text-5xl md:text-6xl font-bold text-center my-10 text-gray-800 fadeInDown animation-slow">
-          About Us
-        </h2>
-        <div className="flex flex-wrap md:flex-nowrap gap-8 items-center">
-          <div className="w-full md:w-1/2 fadeInLeft animation-fast">
-            <img
-              src={image}
-              alt="About Us"
-              className="rounded-lg shadow-lg animate-fadeInLeft animation-delay-1s"
-            />
-          </div>
-          <div className="w-full md:w-1/2 fadeInUp animation-delay-2s">
-            <p className="text-lg text-gray-700">
-              Founded with the vision to simplify beauty care, Beauty Booker is your first online destination for discovering and scheduling top-notch beauty services. We connect you with expert professionals to enhance your beauty and wellness journey, making self-care accessible and convenient. Join our community and experience the transformative power of looking and feeling your best.
-            </p>
-          </div>
+    <section className="container mx-auto px-4 py-10">
+      <h2 className="text-5xl md:text-6xl font-bold text-center my-10 text-gray-800" style={{ fontFamily: "'Great Vibes', cursive" }}>
+        About Us
+      </h2>
+      <div className="flex flex-wrap md:flex-nowrap gap-8 items-center">
+        <div className="w-full md:w-1/2">
+          <img
+            src={image}
+            alt="About Us"
+            className="rounded-lg shadow-lg animate-fadeInLeft"
+          />
         </div>
-      </section>
-    </>
+        <div className="w-full md:w-1/2">
+        <p className="text-lg text-gray-700"
+             style={{ fontFamily: "'Unbounded', sans-serif" }}>
+            Beauty Booker is dedicated to providing you with an effortless way to book and experience beauty treatments. Our curated selection ensures you receive top-tier service, tailored to your individual style and preferences. Join us and find out how we can amplify your natural beauty and elevate your daily routine.
+          </p>
+        </div>
+      </div>
+    </section>
   );
 };
 
